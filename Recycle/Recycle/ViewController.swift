8166ac6dfbda76c805c100bbf07fc2c3ca87a203
Jeffery <jefferyho1898@gmail.com>
2020-02-08 15:18:24 -0800

    "First commit to Feature"

---
+++ Recycle/Recycle/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Recycle
+//
+//  Created by Jeffery Ho on 2/8/20.
+//  Copyright © 2020 SLOWip. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // here is where we start up the camera
+        
+        let captureSesssion = AVCaptureSession()
+        
+        guard let captureDevice =
+            AVCaptureDevice.default(for: .video) else {return}
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        captureSesssion.addInput(input)
+        captureSesssion.startRunning()
+    }
+}
+
