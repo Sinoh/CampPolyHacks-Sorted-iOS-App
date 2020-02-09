1535ba2cbf526eb6817be0fda5fa051a2801f5fc
Jeffery <jefferyho1898@gmail.com>
2020-02-08 20:39:16 -0800

    Save Photo is Working

--- Recycle/Recycle/PhotoViewController.swift
+++ Recycle/Recycle/PhotoViewController.swift
@@ -25,6 +25,11 @@ class PhotoViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     /*
     // MARK: - Navigation
 
--- Recycle/Recycle/ViewController.swift
+++ Recycle/Recycle/ViewController.swift
@@ -7,49 +7,7 @@
 //
 
 import UIKit
-//import AVKit
 import AVFoundation
-/*
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // here is where we start up the camera
-        
-        let captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .photo
-        
-        guard let captureDevice =
-            AVCaptureDevice.default(for: .video) else {return}
-        
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
-        
-        captureSession.addInput(input)
-        captureSession.startRunning()
-        
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        
-        
-        let dataOutput = AVCaptureVideoDataOutput()
-        
-        dataOutput.videoSettings = [((kCVPixelBufferPixelFormatTypeKey as NSString) as String): NSNumber(value: kCVPixelFormatType_30RGB)]
-        
-        
-        dataOutput.alwaysDiscardsLateVideoFrames = true
-        if captureSession.canAddOutput(dataOutput) {
-            captureSession.addOutput(dataOutput)
-        }
-        
-        captureSession.commitConfiguration()
-
-    }
-}
- */
-
-
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
@@ -62,6 +20,10 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         prepareCamera()
     }
     
@@ -101,18 +63,11 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     func prepareCamera() {
         captureSession.sessionPreset = AVCaptureSession.Preset.photo
         
-        /*if let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices {
-            captureDevice = availableDevices.first
-            beginSession()
-         
-             }
-         }*/
-        
         let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
             captureDevice = availableDevices.first
             beginSession()
     }
-    
+
     @IBAction func takePhoto(_ sender: Any) {
         takePhoto = true
     }
@@ -121,11 +76,11 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         if takePhoto {
             takePhoto = false
             if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
-                
-                let photoVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PhotoVC") as! PhotoViewController
-                photoVC.takenPhoto = image
-                
+            
                 DispatchQueue.main.async {
+                    let photoVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PhotoVC") as! PhotoViewController
+                    photoVC.takenPhoto = image
+                    
                     self.present(photoVC, animated: true, completion: nil)
                 }
             }
@@ -137,13 +92,19 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
             let context = CIContext()
             
-            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height:  CVPixelBufferGetHeight(pixelBuffer))
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
             
             if let image = context.createCGImage(ciImage, from: imageRect) {
                 return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
             }
+            
         }
         return nil
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 }
